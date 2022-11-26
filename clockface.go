96e1c6abbb016648566e7b8c877da0de398b9b0a
conor.ney <conor.ux@gmail.com>
2022-11-26 12:54:18 -0500

    added unit test for thirtysecondsinradians

--- clockface.go
+++ clockface.go
@@ -1,15 +1,23 @@
 package clockface
 
-import "time"
+import (
+	"time"
+	"math"
 
+)
 // A Point represents a two dimensional Cartesian coordinate
-type Point struct {
-    X float64
-    Y float64
-}
+// type Point struct {
+//     X float64
+//     Y float64
+// }
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
-func SecondHand(t time.Time) Point {
-    return Point{150, 60}
+// func SecondHand(t time.Time) Point {
+//     return Point{150, 60}
+// }
+
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
 }
--- clockface_test.go
+++ clockface_test.go
@@ -1,18 +1,40 @@
-package clockface_test
+package clockface
 
 import (
     "testing"
     "time"
-	"clockface"
+	"math"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-    tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// func TestSecondHandAtMidnight(t *testing.T) {
+//     tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-    want := clockface.Point{X: 150, Y: 150 - 90}
-    got := clockface.SecondHand(tm)
+//     want := clockface.Point{X: 150, Y: 150 - 90}
+//     got := clockface.SecondHand(tm)
 
-    if got != want {
-        t.Errorf("Got %v, wanted %v", got, want)
-    }
+//     if got != want {
+//         t.Errorf("Got %v, wanted %v", got, want)
+//     }
+// }
+
+// func TestSecondHandAtThirtySeconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := clockface.Point{X: 150, Y: 150 + 90}
+//     got := clockface.SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+// }
+
+
+func TestThirtySecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(2021, time.November, 26, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if got != want {
+		t.Fatalf("got %v want %v", got, want)
+	}
 }
