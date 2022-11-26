511320b5de55003363f0424c73edcfd5000911a5
conor.ney <conor.ux@gmail.com>
2022-11-26 13:40:07 -0500

    refactored secondsInRadians() equation to maintain float64 equality

--- clockface.go
+++ clockface.go
@@ -1,23 +1,26 @@
 package clockface
 
 import (
-	"time"
 	"math"
-
+	"time"
 )
+
 // A Point represents a two dimensional Cartesian coordinate
-// type Point struct {
-//     X float64
-//     Y float64
-// }
+type Point struct {
+    X float64
+    Y float64
+}
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
-// func SecondHand(t time.Time) Point {
-//     return Point{150, 60}
-// }
+func SecondHand(t time.Time) Point {
+    return Point{150, 60}
+}
 
 
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi
+	// by structuring the equation so we don't divide down and then multiply up with Pi,
+	// we are able to preserve the float64 equality, which we otherwise wouldve lost. 
+	// aka Pi / 30 != 30 * (Pi / 30)
+	return (math.Pi / (30 / float64(t.Second())))
 }
--- clockface_test.go
+++ clockface_test.go
@@ -1,40 +1,41 @@
 package clockface
 
 import (
-    "testing"
-    "time"
 	"math"
+	"testing"
+	"time"
 )
 
-// func TestSecondHandAtMidnight(t *testing.T) {
-//     tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func simpleTime (hour, min, sec int) time.Time {
+	return time.Date(2021, time.November, 26, hour, min, sec, 0, time.UTC)
+}
 
-//     want := clockface.Point{X: 150, Y: 150 - 90}
-//     got := clockface.SecondHand(tm)
-
-//     if got != want {
-//         t.Errorf("Got %v, wanted %v", got, want)
-//     }
-// }
-
-// func TestSecondHandAtThirtySeconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-//     got := clockface.SecondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("got %v want %v", got, want)
-// 	}
-// }
+func testName(time time.Time) string {
+	return time.Format("15:04:05")
+}
 
 
-func TestThirtySecondsInRadians(t *testing.T) {
-	thirtySeconds := time.Date(2021, time.November, 26, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := secondsInRadians(thirtySeconds)
+func TestSecondsInRadians(t *testing.T) {
 
-	if got != want {
-		t.Fatalf("got %v want %v", got, want)
+	cases := []struct {
+		time time.Time
+		angle float64
+	} {
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+
 	}
+	
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+
+			if got != c.angle {
+				t.Fatalf("got %v want%v", got, c.angle)
+			}
+		})
+	}
+
 }
