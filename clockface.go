a5954235e9df223a1b7bedb9ea3b0bd11e137f68
duexcoast <conor.ux@gmail.com>
2022-12-07 15:23:12 -0500

    refactored SVGWriter into clockface.go with passing acceptance test for seconds at midnight

--- clockface.go
+++ clockface.go
@@ -1,37 +1,55 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
 
 // A Point represents a two dimensional Cartesian coordinate
 type Point struct {
-    X float64
-    Y float64
+	X float64
+	Y float64
 }
 
 const secondHandLength = 90
 const clockCenterX = 150
-const clockCentersY = 150
+const clockCenterY = 150
+
+// SVGWriter writes an SVG representation of an analogue clock, showing the time t, to the writer w
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
-func SecondHand(t time.Time) Point {
+func secondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}  // scale
-	p = Point{p.X, -p.Y}  // flip
-	p = Point{p.X + clockCenterX, p.Y + clockCentersY}  // translate
-	
-	return p
-
-    return Point{150, 60}
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
 
 func secondsInRadians(t time.Time) float64 {
 	// by structuring the equation so we don't divide down and then multiply up with Pi,
-	// we are able to preserve the float64 equality, which we otherwise wouldve lost. 
+	// we are able to preserve the float64 equality, which we otherwise wouldve lost.
 	// aka Pi / 30 != 30 * (Pi / 30)
 	return (math.Pi / (30 / float64(t.Second())))
 }
---
+++ test.js
@@ -0,0 +1,8 @@
+function hello(x, y) {
+  if (x > y){
+    return x
+  } else {
+    return y - 1
+  }
+}
+hello(75, 30)
